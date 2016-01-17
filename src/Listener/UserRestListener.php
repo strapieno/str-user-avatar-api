9084340e0e2f99a591c381cb4d930361e60b8d76
visa4 <antonino.visalli@gmail.com>
2016-01-17 18:56:14 +0100

    Add listener aggregate

---
+++ src/Listener/UserRestListener.php
@@ -0,0 +1,12 @@
+<?php
+namespace Strapieno\UserAvatar\Api\Listener;
+
+use Zend\EventManager\ListenerAggregateInterface;
+
+/**
+ * Class UserRestListener
+ */
+class UserRestListener implements ListenerAggregateInterface
+{
+
+}
