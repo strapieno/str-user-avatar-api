e034179837f3c85e97b433be618efcec045a9e8c
visa4 <antonino.visalli@gmail.com>
2016-01-21 16:15:33 +0100

    Fix config

--- config/module.config.php
+++ config/module.config.php
@@ -1,44 +1,37 @@
 <?php
 return [
-    'imgman_mongodb' => [
-        'Mongo\Db\Image' => [
-            'database' => 'strapieno-image',
+    'service_manager' => [
+        'factories' => [
+            'Strapieno\Utils\Listener\ListenerManager' => 'Strapieno\Utils\Listener\ListenerManagerFactory'
         ],
-    ],
-    'imgman_adapter_mongo' => [
-        'ImgMan\Mongo\UserAvatar' => [
-            'collection' => 'user_avatar',
-            'database' => 'Mongo\Db\Image',
-            'identifier' => 'identifier'
+        'invokables' => [
+            'Strapieno\Utils\Delegator\AttachListenerDelegator' =>  'Strapieno\Utils\Delegator\AttachListenerDelegator'
         ],
-    ],
-    'imgman_services' => [
-        'ImgMan\Service\UserAvatar' => [
-            'adapter' => 'ImgMan\Adapter\Imagick',
-            'storage' => 'ImgMan\Mongo\UserAvatar',
-            'helper_manager' => 'ImgMan\PluginManager',
-            'renditions' => [
-                'thumb' => [
-                    'fitOut' => [
-                        'width' => 400,
-                        'height' => 400
-                    ],
-                    'format' => [
-                        'format' => 'jpeg'
-                    ],
-                ],
-                'normal' => [
-                    'fitOut' => [
-                        'width' => 30,
-                        'height' => 30,
-                        'allowUpsample' => true
-                    ],
-                    'format' => [
-                        'format' => 'jpeg'
-                    ]
-                ]
-            ]
+        'aliases' => [
+            'listenerManager' => 'Strapieno\Utils\Listener\ListenerManager'
         ]
+    ],
+    // Register listener to listener manager
+    'service-listeners' => [
+        'initializers' => [
+            'Strapieno\User\Model\UserModelInizializer'
+        ],
+        'invokables' => [
+            'Strapieno\UserAvatar\Api\Listener\UserRestListener'
+                => 'Strapieno\UserAvatar\Api\Listener\UserRestListener'
+        ]
+    ],
+    'attach-listeners' => [
+        'Strapieno\UserAvatar\Api\V1\Rest\Controller' => [
+            'Strapieno\UserAvatar\Api\Listener\UserRestListener'
+        ]
+    ],
+    'controllers' => [
+        'delegators' => [
+            'Strapieno\UserAvatar\Api\V1\Rest\Controller' => [
+                'Strapieno\Utils\Delegator\AttachListenerDelegator',
+            ]
+        ],
     ],
     'router' => [
         'routes' => [
--- src/Listener/UserRestListener.php
+++ src/Listener/UserRestListener.php
@@ -1,12 +1,84 @@
 <?php
 namespace Strapieno\UserAvatar\Api\Listener;
 
+use Matryoshka\Model\Object\ActiveRecord\ActiveRecordInterface;
+use Matryoshka\Model\Wrapper\Mongo\Criteria\ActiveRecord\ActiveRecordCriteria;
+use Strapieno\User\Model\Entity\UserInterface;
+use Strapieno\UserAvatar\Model\Entity\UserAvatarAwareInterface;
+use Zend\EventManager\Event;
+use Zend\EventManager\EventManagerInterface;
 use Zend\EventManager\ListenerAggregateInterface;
+use Zend\EventManager\ListenerAggregateTrait;
+use Zend\ServiceManager\AbstractPluginManager;
+use Zend\ServiceManager\ServiceLocatorAwareTrait;
 
 /**
  * Class UserRestListener
  */
 class UserRestListener implements ListenerAggregateInterface
 {
+    use ListenerAggregateTrait;
+    use ServiceLocatorAwareTrait;
 
+    /**
+     * {@inheritdoc}
+     */
+    public function attach(EventManagerInterface $events)
+    {
+        $this->listeners[] = $events->attach('update.post', [$this, 'onPostUpdate']);
+        $this->listeners[] = $events->attach('delete.post', [$this, 'onPostDelete']);
+    }
+
+    /**
+     * @param Event $e
+     */
+    public function onPostUpdate(Event $e)
+    {
+        $serviceLocator = $this->getServiceLocator();
+        if ($serviceLocator instanceof AbstractPluginManager) {
+            $serviceLocator = $serviceLocator->getServiceLocator();
+        }
+
+        $id  = $e->getParam('id');
+        $user = $this->getUserFromId($id);
+
+        if ($user instanceof UserAvatarAwareInterface && $user instanceof ActiveRecordInterface) {
+
+            /** @var $router RouteInterface */
+            $router = $serviceLocator->get('Router');
+            $url = $router->assemble(
+                ['user_id' => $id],
+                ['name' => 'api-rest/user/avatar', 'force_canonical' => true]
+            );
+
+            $user->setAvatar($url);
+            $user->save();
+        }
+    }
+
+    /**
+     * @param Event $e
+     */
+    public function onPostDelete(Event $e)
+    {
+
+        $id  = $e->getParam('id');
+        $user = $this->getUserFromId($id);
+
+        if ($user instanceof UserAvatarAwareInterface && $user instanceof ActiveRecordInterface) {
+
+            $user->setAvatar(null);
+            $user->save();
+        }
+    }
+
+    /**
+     * @param $id
+     * @return UserInterface|null
+     */
+    protected function getUserFromId($id)
+    {
+        return $this->getUserModelService()->find((new ActiveRecordCriteria())->setId($id))->current();
+
+    }
 }
