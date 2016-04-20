e484958f1ff2d75f0ba1e1890880330bc46ba2b7
visa4 <antonino.visalli@gmail.com>
2016-04-20 12:26:27 +0200

    Fix config

--- config/module.config.php
+++ config/module.config.php
@@ -109,7 +109,7 @@ return [
             'input_filter' => 'UserAvatarInputFilter',
         ],
     ],
-    'input_filter_specs' => [
+    'strapieno_input_filter_specs' => [
         'UserAvatarInputFilter' => [
             [
                 'name' => 'blob',
