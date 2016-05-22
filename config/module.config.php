6ea3f1466706574bc43dde1e31df9fbeb065eb6d
visa4 <antonino.visalli@gmail.com>
2016-05-22 22:18:30 +0200

    Fix config

--- config/module.config.php
+++ config/module.config.php
@@ -98,7 +98,7 @@ return [
      'zf-hal' => [
         // map each class (by name) to their metadata mappings
        'metadata_map' => [
-            'Strapieno\Utils\Model\Entity\ImgMan\ImgManEntity' => [
+            'Strapieno\UserAvatar\Model\Entity\UserAvatarEntity' => [
                 'entity_identifier_name' => 'id',
                 'route_name' => 'api-rest/user/avatar',
                 'route_identifier_name' => 'user_id',
